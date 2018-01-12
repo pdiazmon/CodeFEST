eea6cf32be8e4455fc9da6d3c40f7134dcc40b60
pdiazmon <pedroluis.diaz@gmail.com>
2018-01-12 10:54:18 +0100

    Initial Commit

---
+++ CodeFEST/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodeFEST
+//
+//  Created by Pedro L. Diaz Montilla on 12/1/18.
+//  Copyright © 2018 Pedro L. Diaz Montilla. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
